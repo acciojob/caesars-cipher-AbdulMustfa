82010320b859de8ac327d2d7af12fda35b65e0fa
Abdul Mustafa <117674574+AbdulMustfa@users.noreply.github.com>
2023-04-14 22:15:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ function rot13(Str){
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("EBG13 rknzcyr.")); // Output: "ROT13 example."
 
-module.exports = rot13;
+
+module.export = rot13;
